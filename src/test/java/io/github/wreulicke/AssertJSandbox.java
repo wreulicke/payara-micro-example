83f69e584501c9ee48810bc0fcfe8338535c9222
Wreulicke <masaya2dies@gmail.com>
2016-11-24 02:04:19 +0900

    fix test case

--- src/test/java/io/github/wreulicke/AssertJSandbox.java
+++ src/test/java/io/github/wreulicke/AssertJSandbox.java
@@ -15,18 +15,15 @@ import io.github.wreulicke.application.User;
 
 public class AssertJSandbox {
   @Test
-  public void test(){
-    System.out.println("aaaaaaaa");
-    assertThat(false).isEqualTo(true);
-  }
-  
-  @Test
   public void jacksonTest() throws JsonParseException, JsonMappingException, IOException{
     ObjectMapper mapper = new ObjectMapper();
     TestBean bean=mapper.readValue("{\"x\":2,\"y\":3}", TestBean.class);
+
+    assertThat(bean.v1).isEqualTo(2);
+    assertThat(bean.v2).isEqualTo(3);
     System.out.println(bean.v1+" "+bean.v2);
     User user=mapper.readValue("{\"name\":\"sss\"}", User.class);
-    System.out.println(mapper);
+    assertThat(user.getName()).isEqualTo("sss");
   }
   static class TestBean {
     private int v1, v2;
