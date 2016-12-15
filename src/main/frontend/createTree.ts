7a821150b3004a253101da8f0a936d12c2d6b4b8
Wreulicke <masaya2dies@gmail.com>
2016-12-16 04:31:21 +0900

    update TreeView

---
+++ src/main/frontend/createTree.ts
@@ -0,0 +1,34 @@
+
+interface Task {
+  id: number
+  name: string
+  parent?: number
+}
+
+interface TaskNode extends Task {
+  children?: TaskNode[]
+}
+
+type TaskRepository = { [id: string]: TaskNode }
+
+
+const createTreeInternal = (tree: TaskRepository, task: Task) => {
+  const ref = tree[task.id] = {
+    ...tree[task.id],
+    id: task.id,
+    name: task.name,
+    parent: task.parent,
+  }
+  if (task.parent != null) {
+    const parent = tree[task.parent] = tree[task.parent] || { id: task.parent }
+    parent.children = (parent.children || []).concat(ref)
+  }
+  return tree
+}
+
+const createTree = (tasks: Task[]) => {
+  const result = tasks.reduce(createTreeInternal, {})
+  return Object.keys(result).map((key) => result[key]).filter((task) => task.parent == null)
+}
+
+export default createTree
---
+++ src/main/frontend/tasks.ts
@@ -0,0 +1,63 @@
+const tasks = [
+  {
+    id: 1,
+    name: "My Tree",
+  },
+  {
+    name: "hello",
+    id: 2,
+    parent: 1,
+  },
+  {
+    name: "wat",
+    id: 3,
+    parent: 1,
+  },
+  {
+    id: 4,
+    name: "child folder",
+    parent: 1,
+  },
+  {
+    id: 5,
+    name: "child folder",
+    parent: 4,
+  },
+  {
+    name: "hello",
+    id: 6,
+    parent: 5,
+  },
+  {
+    name: "wat",
+    id: 7,
+    parent: 5,
+  },
+  {
+    name: "hello",
+    id: 8,
+    parent: 4,
+  },
+  {
+    name: "wat",
+    id: 9,
+    parent: 4,
+  },
+  {
+    id: 10,
+    name: "child folder",
+    parent: 4,
+  },
+  {
+    name: "hello",
+    id: 11,
+    parent: 10,
+  },
+  {
+    name: "wat",
+    id: 12,
+    parent: 10,
+  },
+]
+
+export default tasks
